41fc3b5d39e302632b1ce56cbbc310b46548833e
Brandon Mitchell <brandon@twitter.com>
2010-08-30 13:29:36 -0700

    MockService is no longer a singleton.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,11 +9,8 @@ require 'flock/mock_service'
 
 Spec::Runner.configure do |config|
   config.mock_with :rr
-  config.before do
-    Flock::MockService.clear
-  end
 end
 
 def new_flock_client
-  Flock.new(Flock::MockService)
+  Flock.new(Flock::MockService.new)
 end
