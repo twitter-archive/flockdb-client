472d086f0d0eb52576e5e5660903688ef0570e0b
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-10 14:29:31 -0800

    move execute_at and position to an options hash

--- lib/flock/operations/execute_operations.rb
+++ lib/flock/operations/execute_operations.rb
@@ -1,12 +1,12 @@
 module Flock
   class ExecuteOperations
     def initialize(service, priority, execute_at = nil)
-      @service, @operations, @priority, @execute_at = service, [], priority, execute_at 
+      @service, @operations, @priority, @execute_at = service, [], priority, execute_at
     end
 
     Flock::Edges::ExecuteOperationType::VALUE_MAP.each do |op_id, op|
       op = op.downcase
-      class_eval "def #{op}(s, g, d); @operations << ExecuteOperation.new(#{op_id}, [s, g, d]); self end", __FILE__, __LINE__
+      class_eval "def #{op}(s, g, d, p = nil); @operations << ExecuteOperation.new(#{op_id}, [s, g, d], p); self end", __FILE__, __LINE__
     end
 
     def apply
--- spec/execute_operations_spec.rb
+++ spec/execute_operations_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe Flock::ExecuteOperations do
   describe "#negate" do
     it "should create a negate opperation" do
-      mock(Flock::ExecuteOperation).new(Flock::Edges::ExecuteOperationType::Negate, [1,1,3])
+      mock(Flock::ExecuteOperation).new(Flock::Edges::ExecuteOperationType::Negate, [1,1,3], nil)
       operation = Flock::ExecuteOperations.new(Flock::Client.new(Flock::MockService), nil)
       operation.negate(1,1,3)
     end
   end
-  
+
   describe "adding with timestamp" do
     it "should pass through" do
       service = Flock::MockService
@@ -16,7 +16,19 @@ describe Flock::ExecuteOperations do
       now = Time.now.to_i
       thing = Flock::ExecuteOperations.new(service, Flock::Priority::High, now)
       mock(Flock::ExecuteOperations).new(service, Flock::Priority::High, now) { thing }
-      client.add(1, 1, 3, Flock::Priority::High, now)
+      client.add(1, 1, 3, Flock::Priority::High, :execute_at => now)
     end
   end
+
+  describe "adding with position" do
+    it "should pass through" do
+      service = Flock::MockService
+      client = Flock::Client.new(service)
+      now = Time.now.to_i
+      thing = Flock::ExecuteOperation.new(1, [1, 1, 3], 12345)
+      mock(Flock::ExecuteOperation).new(1, [1, 1, 3], 12345) { thing}
+      client.add(1, 1, 3, Flock::Priority::High, :position => 12345)
+    end
+  end
+
 end
